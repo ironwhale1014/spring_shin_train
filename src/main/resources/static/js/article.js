57fc47a3a0eae663f781201c438f63a2d2234351
King <mac@mac.com>
2024-01-26 12:50:10 +0900

    생성 까지 오류 해결 필요

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,3 +14,66 @@ if (deleteButton) {
         })
     })
 }
+
+const modifyButton = document.getElementById('modify-btn')
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', ev => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`api/articles/${id}`, {
+            method: 'PUT', headers: {
+                "Content-Type": "application/json",
+            }, body: JSON.stringify({
+                title: document.getElementById('title').value, content: document.getElementById('content').value
+            })
+
+        }).then(() => {
+            alert('수정이 완료되었습니다. ');
+            location.replace(`/articles/${id}`);
+        });
+    });
+}
+
+// const createButton = document.getElementById("create-btn");
+// console.log("createButton")
+// if (createButton) {
+//     createButton.addEventListener("click", ev => {
+//         fetch("api/articles", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 title: document.getElementById("title").value,
+//                 content: document.getElementById("content").value
+//             })
+//         }).then(() => {
+//             alert("등록 완료되었습니다.");
+//             location.replace("/articles");
+//         });
+//     });
+// }
+
+// // 생성 기능
+// const createButton = document.getElementById('create-btn');
+//
+// if (createButton) {
+//     createButton.addEventListener('click', event => {
+//         fetch('/api/articles', {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 title: document.getElementById('title').value,
+//                 content: document.getElementById('content').value
+//             })
+//         })
+//             .then(() => {
+//                 alert('등록 완료되었습니다.');
+//                 location.replace('/articles');
+//             });
+//     });
+// }
