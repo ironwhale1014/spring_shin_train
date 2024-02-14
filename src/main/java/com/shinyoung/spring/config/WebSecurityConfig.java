32e4f786a77f49ad1e577f8e91d281f380de9aab
King <mac@mac.com>
2024-02-15 06:54:39 +0900

    시큐리티 시작

---
+++ src/main/java/com/shinyoung/spring/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.shinyoung.spring.config;
+
+import com.shinyoung.spring.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final UserDetailService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring().requestMatchers(toH2Console()).requestMatchers("/static/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests.requestMatchers("/login", "/signup", "/user")
+                                .permitAll().anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin.loginPage("/login").defaultSuccessUrl("/articles"))
+                .logout(logout -> logout.logoutSuccessUrl("/login").invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+                                                       UserDetailService userDetailService) throws Exception {
+        AuthenticationManagerBuilder sharedObject = http.getSharedObject(AuthenticationManagerBuilder.class);
+        sharedObject.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return sharedObject.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/shinyoung/spring/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package com.shinyoung.spring.service;
+
+import com.shinyoung.spring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
